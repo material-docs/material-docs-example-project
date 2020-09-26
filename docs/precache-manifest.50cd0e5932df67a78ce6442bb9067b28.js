28bf598219d6f537a0bf20c2cfc092f7f36c2386
DanilAndreev <33941552+DanilAndreev@users.noreply.github.com>
2020-09-26 23:08:08 +0300

    [GitHub Pages] Added config.

---
+++ docs/precache-manifest.50cd0e5932df67a78ce6442bb9067b28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e35650a725820acacc1ef0f91fb2da73",
+    "url": "/material-docs-example-project/index.html"
+  },
+  {
+    "revision": "4479b36811fe1e4d1ed8",
+    "url": "/material-docs-example-project/static/js/2.2ae444b5.chunk.js"
+  },
+  {
+    "revision": "09462af000b2c6ea38a0a4d28c19006a",
+    "url": "/material-docs-example-project/static/js/2.2ae444b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e62a1727326f8f338ff",
+    "url": "/material-docs-example-project/static/js/main.a8bcb4b3.chunk.js"
+  },
+  {
+    "revision": "9d46efa53419240dfd40",
+    "url": "/material-docs-example-project/static/js/runtime-main.79214dd2.js"
+  }
+]);
