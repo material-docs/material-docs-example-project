f93bd172a4bb2bead1a248a9cbe74184d56c752c
DanilAndreev <33941552+DanilAndreev@users.noreply.github.com>
2020-09-26 22:55:32 +0300

    [GitHub Pages] Added config.

---
+++ docs/precache-manifest.cbc16a60395058927ee4d0e70c006276.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9cd2483c8cde08fd1ec6bf72a50f4a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4479b36811fe1e4d1ed8",
+    "url": "/static/js/2.2ae444b5.chunk.js"
+  },
+  {
+    "revision": "09462af000b2c6ea38a0a4d28c19006a",
+    "url": "/static/js/2.2ae444b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4e99ab00f17e0040ecf",
+    "url": "/static/js/main.66962745.chunk.js"
+  },
+  {
+    "revision": "9cfa404a5f810f3f4ccd",
+    "url": "/static/js/runtime-main.4a837d06.js"
+  }
+]);
